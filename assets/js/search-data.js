a87858d84ce09754fc50e611b4ec64d7123efa4b
haoyang504 <haoyangwang@utexas.edu>
2025-09-19 06:23:08 +0000

    Deploying to gh-pages from @ haoyang504/haoyang504.github.io@5da2b1973d463f58bab4d077558600aa2b41c009 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -448,8 +448,8 @@ ninja.data = [{
           description: "",
           section: "News",handler: () => {
               window.location.href = "/news/announcement_2/";
-            },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
-          title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
+            },},{id: "news-first-try-to-modify-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
+          title: 'First try to modify! A simple inline announcement with Markdown emoji! :sparkles: :smile:...',
           description: "",
           section: "News",},{id: "projects-project-1",
           title: 'project 1',
